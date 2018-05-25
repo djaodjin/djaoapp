59d5d04787f79909157eb1a9025276b048d40394
Dima Knivets <knivets@gmail.com>
2018-05-25 15:05:23 +0300

    current ui progress

--- djaoapp/static/js/djaodjin-dashboard.js
+++ djaoapp/static/js/djaodjin-dashboard.js
@@ -9,36 +9,23 @@
     "use strict";
 
 $(document).ready(function(){
-    $(".dashboard-nav-toggle").click(function() {
-        var element = $(this);
-        var icon = element.children("i");
-        var nav = element.parents(".dashboard-nav");
-        var containerInner = nav.parents(".dashboard-inner-container");
-        var navWidth = nav.width();
-        var toggleWidth = element.width();
-        var sidebarEdge = containerInner.css("left");
-        icon.removeClass();
-        if( sidebarEdge === "-" + (navWidth - toggleWidth) + "px" ) {
-            sidebarEdge = 0;
-            icon.addClass("fa fa-angle-double-left");
-        } else {
-            sidebarEdge = "-" + (navWidth - toggleWidth) + "px";
-            icon.addClass("fa fa-angle-double-right");
-        }
-        var animates = {left: sidebarEdge};
-        var container =  containerInner.parents(".dashboard-container");
-        var containerW = container.width();
-        if( containerW >= 768 ) {
-            if( sidebarEdge ) {
-                var newInnerWidth = containerW + (navWidth - toggleWidth);
-                animates['width'] = "" + newInnerWidth + "px";
-            } else {
-                animates['width'] = "" + containerW + "px";
-            }
-        }
-        containerInner.animate(animates, 500, function() {
-            container.trigger("resize");
-        });
+    var body = $('body');
+    var nav = $('.dashboard-nav');
+    var overlay = $('.dashboard-overlay')
+    $(".dashboard-header-menu .navbar-toggle").click(function() {
+        body.css('overflow', 'hidden')
+        overlay.show()
+        nav.fadeIn(100);
+    });
+    overlay.click(function() {
+        overlay.hide()
+        body.css('overflow', 'visible')
+        nav.fadeOut(100);
+    });
+    $(window).resize(function() {
+        overlay.hide()
+        body.css('overflow', 'visible')
+        nav.hide();
     });
 });
 
