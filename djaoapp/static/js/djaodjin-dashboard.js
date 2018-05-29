84ca733dad1d091c858aad2374ea7df39894a1fe
Dima Knivets <knivets@gmail.com>
2018-05-29 15:07:12 +0300

    sliding sidebar and close button

--- djaoapp/static/js/djaodjin-dashboard.js
+++ djaoapp/static/js/djaodjin-dashboard.js
@@ -11,21 +11,23 @@
 $(document).ready(function(){
     var body = $('body');
     var nav = $('.dashboard-nav');
-    var overlay = $('.dashboard-overlay')
+    var overlay = $('.dashboard-overlay');
+    var content = $('.dashboard-content');
     $(".dashboard-header-menu .navbar-toggle").click(function() {
+        content.animate({left: '220px'})
         body.css('overflow', 'hidden')
         overlay.show()
-        nav.fadeIn(100);
+        nav.animate({left: '0'});
     });
-    overlay.click(function() {
+    overlay.add('.collapse-sidebar').click(function() {
         overlay.hide()
         body.css('overflow', 'visible')
-        nav.fadeOut(100);
+        nav.animate({left: '-350px'})
+        content.animate({left: 0})
     });
     $(window).resize(function() {
         overlay.hide()
-        body.css('overflow', 'visible')
-        nav.hide();
+        $(body).add(nav).add(content).removeAttr('style')
     });
 });
 
