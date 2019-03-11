954be8f696efc5c9c16a5635b3dffd5aeca0e069
Dima Knivets <knivets@gmail.com>
2019-03-11 15:57:28 +0200

    show/hide edit tools toggle #37

--- djaoapp/views/custom_themes.py
+++ djaoapp/views/custom_themes.py
@@ -1,20 +1,32 @@
 # Copyright (c) 2019, DjaoDjin inc.
 # see LICENSE
 
+from django.http import HttpResponseRedirect
 from multitier.mixins import build_absolute_uri
 from multitier.thread_locals import get_current_site
 from pages.views.themes import (
     ThemePackagesView as  ThemePackageBaseView,
     ThemePackageDownloadView as ThemePackageDownloadBaseView)
 
+from ..compat import reverse
+
 
 class ThemePackageView(ThemePackageBaseView):
 
     def get_context_data(self, **kwargs):
         context = super(ThemePackageView, self).get_context_data(**kwargs)
-        context.update({'site_available_at_url': build_absolute_uri(
-            self.request, site=get_current_site().db_object)})
+        context.update({
+            'site_available_at_url': build_absolute_uri(
+                self.request, site=get_current_site().db_object),
+            'show_edit_tools': self.app.show_edit_tools,
+        })
         return context
+
+    def post(self, request, *args, **kwargs):
+        show = request.POST.get('show_edit_tools') == 'on'
+        self.app.show_edit_tools = show
+        self.app.save()
+        return HttpResponseRedirect(reverse('theme_update'))
 
 
 class ThemePackageDownloadView(ThemePackageDownloadBaseView):
