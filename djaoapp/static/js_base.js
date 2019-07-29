1663f88a02346abfa08e3cc9b6cd6b5c13dd866a
Dima Knivets <knivets@gmail.com>
2019-07-30 00:25:16 +0300

    removed test code

--- djaoapp/static/js_base.js
+++ djaoapp/static/js_base.js
@@ -3,10 +3,5 @@ import 'script-loader!jquery.cookie';
 import 'script-loader!bootstrap';
 import 'script-loader!bootbox';
 import 'script-loader!toastr';
-import 'script-loader!./js/djaodjin-dashboard';
-import 'script-loader!./js/djaodjin-menubar';
-
-jQuery.each([1,2], function(e){
-    console.log(e);
-});
-//console.log($.cookie);
+import 'script-loader!js/djaodjin-dashboard';
+import 'script-loader!js/djaodjin-menubar';
