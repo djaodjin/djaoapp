f8d43a3670f51728906754e5d611dfa6f21d4dcf
Sebastien Mirolo <smirolo@djaodjin.com>
2019-12-16 12:22:33 -0800

    adds Object.assign polyfill for bootstrap-vue
    Phantomjs is not in the supported browsers by bootstrap-vue
    See [Build variants](https://bootstrap-vue.js.org/docs/)

--- djaoapp/static/js/djaodjin-menubar.js
+++ djaoapp/static/js/djaodjin-menubar.js
@@ -4,6 +4,39 @@
 
 /* global document jQuery */
 
+// XXX If we don't define this polyfill, bootstrap-vue does not load
+// correctly on phantomjs.
+// We put it here so it gets loaded before `vendor-vue.js`
+if (typeof Object.assign !== 'function') {
+  // Must be writable: true, enumerable: false, configurable: true
+  Object.defineProperty(Object, "assign", {
+    value: function assign(target, varArgs) { // .length of function is 2
+      'use strict';
+      if (target === null || target === undefined) {
+        throw new TypeError('Cannot convert undefined or null to object');
+      }
+
+      var to = Object(target);
+
+      for (var index = 1; index < arguments.length; index++) {
+        var nextSource = arguments[index];
+
+        if (nextSource !== null && nextSource !== undefined) {
+          for (var nextKey in nextSource) {
+            // Avoid bugs when hasOwnProperty is shadowed
+            if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+              to[nextKey] = nextSource[nextKey];
+            }
+          }
+        }
+      }
+      return to;
+    },
+    writable: true,
+    configurable: true
+  });
+}
+
 
 (function ($) {
     "use strict";
