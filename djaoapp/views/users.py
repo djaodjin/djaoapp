e4ba31499ce48dfbbb25a038d4a8fe58e39661f4
Sebastien Mirolo <smirolo@djaodjin.com>
2020-08-27 14:51:32 -0700

    show billing/subscriptions on attached profiles

--- djaoapp/views/users.py
+++ djaoapp/views/users.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2018, DjaoDjin inc.
+# Copyright (c) 2020, DjaoDjin inc.
 # see LICENSE
 from __future__ import unicode_literals
 
@@ -16,7 +16,7 @@ from signup.views.users import (
     UserNotificationsView as UserNotificationsBaseView,
     PasswordChangeView as UserPasswordUpdateBaseView)
 from saas.views.users import ProductListView as UserAccessiblesBaseView
-from saas.utils import update_context_urls
+from saas.utils import is_broker, update_context_urls
 
 from ..compat import reverse
 
@@ -25,6 +25,24 @@ LOGGER = logging.getLogger(__name__)
 
 
 class UserMixin(object):
+    """
+    Sidebar menus related to a user.
+    """
+    user_url_kwarg = 'user'
+
+    @property
+    def attached_organization(self):
+        if not hasattr(self, '_attached_organization'):
+            try:
+                # We use `get` instead of `attached` here because we want
+                # to redirect to the profle page regardless if an organization
+                # with the lookup slug is found.
+                self._attached_organization = Organization.objects.get(
+                    slug=self.kwargs.get(self.user_url_kwarg))
+            except Organization.DoesNotExist:
+                self._attached_organization = None
+        return self._attached_organization
+
 
     def get_context_data(self, **kwargs):
         context = super(UserMixin, self).get_context_data(**kwargs)
@@ -37,25 +55,26 @@ class UserMixin(object):
                     'users_notifications', args=(self.user,)),
                 'profile': reverse('users_profile', args=(self.user,)),
             }})
+        organization = self.attached_organization
+        if organization and not is_broker(organization):
+            # A broker does not have subscriptions.
+
+            # Duplicate code from `saas.extras.OrganizationMixinBase` since
+            # it does not get inherited in the context of a `UserMixin`.
+            update_context_urls(context, {
+                'organization': {
+                    'billing': reverse(
+                        'saas_billing_info', args=(organization,)),
+                    'subscriptions': reverse(
+                        'saas_subscription_list', args=(organization,)),
+                }})
+
         return context
 
 
 class UserProfileView(UserMixin, UserProfileBaseView):
 
     template_name = 'saas/profile/index.html'
-
-    @property
-    def attached_organization(self):
-        if not hasattr(self, '_attached_organization'):
-            try:
-                # We use `get` instead of `attached` here because we want
-                # to redirect to the profle page regardless if an organization
-                # with the lookup slug is found.
-                self._attached_organization = Organization.objects.get(
-                    slug=self.kwargs.get(self.slug_url_kwarg))
-            except Organization.DoesNotExist:
-                self._attached_organization = None
-        return self._attached_organization
 
     def form_valid(self, form):
         # There is something fundamentally wrong if we have an `attached_user`
