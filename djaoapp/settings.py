950ac9da2ccf989a6886b33811fa853931c0a9ed
Sebastien Mirolo <smirolo@djaodjin.com>
2022-07-27 20:06:33 -0700

    reverts uploads prefix

--- djaoapp/settings.py
+++ djaoapp/settings.py
@@ -249,7 +249,7 @@ HTDOCS = os.path.join(BASE_DIR, 'htdocs')
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = os.path.join(HTDOCS, 'uploads', 'media')
+MEDIA_ROOT = os.path.join(HTDOCS, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
