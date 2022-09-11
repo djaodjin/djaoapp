5563c20e03b3d9413f845dbd444d7dfb2c357eb6
Sebastien Mirolo <smirolo@djaodjin.com>
2022-09-11 14:26:27 -0700

    fixes 500 when fetching static assets through proxy ...
    ... and proxy is not running.

--- djaoapp/views/static.py
+++ djaoapp/views/static.py
@@ -1,8 +1,9 @@
 # Copyright (c) 2021, DjaoDjin inc.
 # see LICENSE
 
+from django.views.generic.base import ContextMixin
 from extended_templates.views.static import AssetView as BaseAssetView
 from rules.views.app import AppMixin, SessionProxyMixin
 
-class AssetView(SessionProxyMixin, AppMixin, BaseAssetView):
+class AssetView(SessionProxyMixin, AppMixin, ContextMixin, BaseAssetView):
     pass
