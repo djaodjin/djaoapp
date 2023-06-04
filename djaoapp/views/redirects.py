30da9bc3287ea4906c0783d46bf4e9f04029802b
Sebastien Mirolo <smirolo@djaodjin.com>
2023-06-04 01:25:03 -0700

    increasing flexibility of onboarding workflow

--- djaoapp/views/redirects.py
+++ djaoapp/views/redirects.py
@@ -6,6 +6,7 @@ Redirect for /billing/cart/
 """
 
 from django.contrib.auth import REDIRECT_FIELD_NAME
+from multitier.thread_locals import get_current_site
 from rules.mixins import AppMixin
 from saas.views import OrganizationRedirectView as BaseOrganizationRedirectView
 from saas.views.profile import (
@@ -16,6 +17,16 @@ from signup.decorators import check_email_verified as check_email_verified_base
 class OrganizationCreateView(AppMixin, BaseOrganizationCreateView):
 
     implicit_create_on_none = False
+
+    def get_context_data(self, **kwargs):
+        context = super(OrganizationCreateView, self).get_context_data(
+            **kwargs)
+        # We have to add 'site' to the context here because the connected
+        # profiles page inherits from UserMixin, not OrganizationMixin.
+        context.update({
+            'site': get_current_site(),
+        })
+        return context
 
     def get_implicit_create_on_none(self):
         if self.app and self.app.get_implicit_create_on_none():
@@ -37,4 +48,5 @@ class OrganizationRedirectView(AppMixin, BaseOrganizationRedirectView):
     def get_implicit_create_on_none(self):
         if self.app and self.app.get_implicit_create_on_none():
             return True
-        return self.implicit_create_on_none
+        return super(
+            OrganizationRedirectView, self).get_implicit_create_on_none()
