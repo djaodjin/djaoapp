0a1df83d37a92611f7282e608e53923d38ff12f7
Sebastien Mirolo <smirolo@djaodjin.com>
2024-04-26 17:26:43 -0700

    bumps version

--- djaoapp/__init__.py
+++ djaoapp/__init__.py
@@ -1,5 +1,5 @@
-# Copyright (c) 2023, DjaoDjin inc.
+# Copyright (c) 2024, DjaoDjin inc.
 # see LICENSE
 from __future__ import unicode_literals
 
-__version__ = '2024-03-15-dev'
+__version__ = '2024-03-15.1'
